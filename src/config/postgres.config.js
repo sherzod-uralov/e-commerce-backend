121e1a1a61ec4054c49c37c1cfe0e9ece2043eea
sherzod-uralov <sherzoduralov432@gmail.com>
2024-02-13 21:21:58 +0500

    a

--- src/config/postgres.config.js
+++ src/config/postgres.config.js
@@ -1,15 +1,14 @@
 import { Sequelize } from 'sequelize'
 import 'dotenv/config'
 
-const newSequelize = new Sequelize('postgres://moxkbyjkeknqje:9d6115f87c4c35a3a1a20ea1e5d37299ff55abc8822adf03e25976967e5c6406@ec2-107-21-67-46.compute-1.amazonaws.com:5432/dacqphsq5vqaao',{
-    dialectOptions: {
-        ssl: {
-            require: true, // Agar foydalanuvchi server bilan amal qilmasa, ulgurji serverga ulanishni talab qiladi
-            rejectUnauthorized: false // Lokal yoki test maqsadlar uchun ishlatilgan daqiqo o'zgartiriladi. Ishlab chiqarishga o'tkazish uchun ba'zi foydalanuvchilar tomonidan tavsiya etilmaydi.
-        }
-    },
-    logging: true
+const newSequelize = new Sequelize({
+    database:'shop',
+    username:'postgres',
+    dialect:'postgres',
+    password:'sherzod',
+    host:'localhost',
+    logging:false
 })
  
 export { newSequelize }
- 
+   
